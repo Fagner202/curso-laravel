59b5a9436b1fa01bdc13beb051218cf5ceb79a1d
Fagner202 <fagnersilveira86@gmail.com>
2024-06-01 15:07:00 -0300

    Aula 03
    Trabalhando com rotas

---
+++ resources/views/jogos.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Trabalhando com rotas nas views</h1>
+
+    <a href="{{ route('home-index') }}">Clique aqui!</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
+})->name('home-index');
+
+// Route::view('/jogos', 'jogos');
+
+// Route::get('/jogos', function () {
+//     return "Tá estudando vagabundo";
+// });
+
+// Route::view('/jogos', 'jogos', ['name' => 'GEARS']);
+
+// Route::get('/jogos/{name?}', function($name = null) {
+//     return view('/jogos', ['nomeJogo' => $name]);
+// });
+
+// Route::get('/jogos/{name?}', function($name = null) {
+//     return view('/jogos', ['nomeJogo' => $name]);
+// })->where('name', '[A-Za-z]+');
+
+
+// Route::get('/jogos/{id?}/{name?}', function($id = null, $name = null) {
+//     return view('/jogos', ['idJogo' => $id, 'nameJogo' => $name]);
+// });
+
+Route::get('/jogos', function() {
+    return view('jogos');
 });
